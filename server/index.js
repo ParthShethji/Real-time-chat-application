1bcdfc7053c6e12726bb493f6d977015e1292e06
Parth Shethji <parthshethji2693@gmail.com>
2024-05-06 02:06:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,9 +10,12 @@ app.use(cors());
 require('dotenv').config();
 
 app.use(cors({
-    origin: ["https://real-time-chat-application-frontend.vercel.app],
-    methods: ["POST", "GET"],
-    credentials: True,
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://real-time-chat-application-frontend.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 }));
 app.use(express.json());
 app.use(express.urlencoded());
